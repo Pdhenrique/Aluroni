0b9a71b3e33d6c640fe5f6a1b6cfb7f111256e0c
Pedro Santana <pedroschw.c@gmail.com>
2023-07-10 14:52:16 -0300

    feat: added item list

---
+++ src/pages/Menu/Items /Item/index.tsx
@@ -0,0 +1,40 @@
+import styles from './Item.module.scss'
+import menu from '../Items.json'
+import classNames from 'classnames'
+
+type Props = typeof menu[0]
+
+export default function Item(props: Props) {
+    
+    const {title, description, category, price, size, serving, photo } = props
+
+    return (
+        <div className={styles.item}>
+            <div className={styles.item__image}>
+                <img src={photo} alt={title} />
+            </div>
+            <div className={styles.item__description}>
+                <h2>{title}</h2>
+                <p> {description}</p>
+
+                <div className={styles.item__tags}>
+                    <div className={classNames({
+                        [styles.item__type]: true,
+                        [styles[`item__type__${category.label.toLowerCase()}`]]: true
+                    })}>
+                        {category.label}
+                    </div>
+                    <div className={styles.item__portion}>
+                        {size}
+                    </div>
+                    <div className={styles.item__amtPeople}>
+                        {serving} pessoa{serving === 1 ? '' : "s" }
+                    </div>
+                    <div className={styles.item__value}>
+                        {price.toFixed(2)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
