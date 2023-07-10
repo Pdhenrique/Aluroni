cbc79c3162591cf6023852def67968fe3c96f813
Pedro Santana <pedroschw.c@gmail.com>
2023-07-10 12:06:27 -0300

    feat: added computer component

---
+++ src/pages/Menu/computer/index.tsx
@@ -0,0 +1,19 @@
+import styles from './Computer.module.scss'
+import options from './options.json'
+
+export default function Computer() {
+
+    return(
+        <button className={styles.computer}>
+            <span>Computer by</span>
+            <div className={styles.computer__options}>
+                {options.map(option => (
+                    <div className={styles.computer__option} key={option.value}>
+                        {option.name}
+                    </div>
+                ))}
+            </div>
+        </button>
+    
+    )
+}
