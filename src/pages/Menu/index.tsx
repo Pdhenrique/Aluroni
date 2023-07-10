bb1d5a152ba69104549a01aaad9c27e9b7cccc94
Pedro Santana <pedroschw.c@gmail.com>
2023-07-10 12:08:24 -0300

    refactor: added filter component

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -2,10 +2,12 @@ import styles from './Menu.module.scss'
 import {ReactComponent as Logo} from "assets/Menu/logo.svg"
 import SearchEngine from './SearchEngine'
 import { useState } from 'react'
+import Filter from './Filter'
 
 export default function Menu() {
  
     const [search, setSearch] = useState("")
+    const [filter, setFilter] = useState<number | null>(null)
 
     return(
         <main>
@@ -19,10 +21,13 @@ export default function Menu() {
             </header>
             <section className={styles.menu}>
                 <h3 className={styles.menu__title}>Menu</h3>
-                <SearchEngine
-                search={search}
-                setSearch={setSearch}
-                />
+                <SearchEngine search={search} setSearch={setSearch}/>
+                <div className={styles.menu__filter}>
+                    <Filter
+                    filter={filter}
+                    setFilter={setFilter}
+                    />
+                </div>
             </section>
         </main>
 
