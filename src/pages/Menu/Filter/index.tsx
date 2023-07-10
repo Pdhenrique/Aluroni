fad3b43d1b0bd28ded5cd614a5da8cf24f9bf684
Pedro Santana <pedroschw.c@gmail.com>
2023-07-10 12:06:40 -0300

    feat: added filter component

---
+++ src/pages/Menu/Filter/index.tsx
@@ -0,0 +1,33 @@
+import styles from './Filter.module.scss'
+import filters from './Filter.json';
+import classNames from 'classnames'
+
+type IOption = typeof filters[0]
+
+interface Props {
+    filter: number |  null;
+    setFilter: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filter({filter, setFilter}: Props) {
+    
+    function selectFilter(option: IOption){
+        if(filter === option.id) return setFilter(null);
+        return setFilter(option.id)
+    }
+
+    return( 
+    <div className={styles.filters}>
+        {filters.map((option) => (
+            <button 
+            className={classNames({
+                [styles.filters__filter]: true,
+                [styles['filters__filter--active']]: filter === option.id
+            })}
+            key={option.id} 
+            onClick={() => selectFilter(option)}>
+                {option.label}
+            </button>
+        ))}
+    </div>)
+}
