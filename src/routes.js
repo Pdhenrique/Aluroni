dfb8cbf98c244d6d6110966819eeb9ff2b354e36
Pedro Santana <pedroschw.c@gmail.com>
2023-07-13 13:20:48 -0300

    refactor: updating routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import DefaultPage from 'components/DefaultPage'
+import Footer from 'components/Footer'
 import HeaderNav from 'components/HeaderNav'
+import About from 'pages/About'
 import Home from 'pages/Home'
 import Menu from 'pages/Menu'
+import Dishes from 'pages/dishes'
+import NotFound from 'pages/notFound'
 import { memo } from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function AppRoutes(){
     return(
-        <main>
+        <main className='container'>
             <Router>
                 <HeaderNav/>
                 
@@ -16,8 +20,13 @@ function AppRoutes(){
                     <Route path="/" element={<DefaultPage/>}>
                         <Route index element={<Home/>}/>
                         <Route path="Menu" element={<Menu/>}/>
+                        <Route path="About" element={<About/>}/>
+                        
                     </Route>
+                    <Route path="*" element={<NotFound/>} />
+                    <Route path='dishes/:id' element={<Dishes/>}/>
                 </Routes>
+                <Footer/>
             </Router>
        
         </main>
