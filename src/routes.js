6ea2beafd0b100bc2f0460ce98cfcb52166e65b7
Pedro Santana <pedroschw.c@gmail.com>
2023-07-14 14:46:04 -0300

    feat: adding lazy imports

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,34 @@
-import DefaultPage from 'components/DefaultPage'
 import Footer from 'components/Footer'
 import HeaderNav from 'components/HeaderNav'
-import About from 'pages/About'
-import Home from 'pages/Home'
-import Menu from 'pages/Menu'
-import Dishes from 'pages/dishes'
-import NotFound from 'pages/notFound'
-import { memo } from 'react'
 
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function AppRoutes(){
+const Home = lazy(() => import('pages/Home'))
+const Menu = lazy(() => import('pages/Menu'))
+const About = lazy(() => import('pages/About'))
+const Dishes = lazy(() => import ('pages/dishes'))
+const NotFound = lazy(() => import('pages/notFound'))
+const DefaultPage = lazy(() => import('components/DefaultPage'))
+
+
+export default function AppRoutes(){
     return(
         <main className='container'>
             <Router>
                 <HeaderNav/>
-                
-                <Routes>
-                    <Route path="/" element={<DefaultPage/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path="Menu" element={<Menu/>}/>
-                        <Route path="About" element={<About/>}/>
-                        
-                    </Route>
-                    <Route path="*" element={<NotFound/>} />
-                    <Route path='dishes/:id' element={<Dishes/>}/>
-                </Routes>
-                <Footer/>
+                <Suspense fallback={<p> Loading...</p>}>
+                    <Routes >
+                        <Route path="/" element={<DefaultPage/>}>
+                            <Route index element={<Home/>}/>
+                            <Route path="Menu" element={<Menu/>}/>
+                            <Route path="About" element={<About/>}/> 
+                        </Route>
+                        <Route path="*" element={<NotFound/>} />
+                        <Route path='dishes/:id' element={<Dishes/>}/>
+                    </Routes>
+                </Suspense>
+                <Footer/> 
             </Router>
        
         </main>
@@ -34,4 +36,3 @@ function AppRoutes(){
 }
 
 
-export default memo(AppRoutes)
